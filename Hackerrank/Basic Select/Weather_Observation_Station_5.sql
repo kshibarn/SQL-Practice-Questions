4c1c6da95e527ef15a7bcf0b7d0abde716f37171
KSHITIJ BARNWAL <73215352+kshibarn@users.noreply.github.com>
2022-10-05 15:38:23 +0530

    Added Basic Select Questions

---
+++ Hackerrank/Basic Select/Weather_Observation_Station_5.sql
@@ -0,0 +1,30 @@
+/* Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name). 
+ If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically. */
+/* Sample Input
+ 
+ For example, CITY has four entries: DEF, ABC, PQRS and WXY.
+ 
+ Sample Output
+ 
+ ABC 3
+ PQRS 4
+ */
+SELECT *
+FROM (
+        SELECT DISTINCT city,
+            LENGTH(city)
+        FROM station
+        ORDER BY LENGTH(city) ASC,
+            city ASC
+    )
+WHERE ROWNUM = 1
+UNION
+SELECT *
+FROM (
+        SELECT DISTINCT city,
+            LENGTH(city)
+        FROM station
+        ORDER BY LENGTH(city) DESC,
+            city ASC
+    )
+WHERE ROWNUM = 1;
---
+++ Hackerrank/Basic Select/Weather_Observation_Station_6.sql
@@ -0,0 +1,9 @@
+/* Query the list of CITY names starting with vowels (i.e., a, e, i, o, OR u) from STATION. 
+ Your result cannot contain duplicates. */
+SELECT DISTINCT city
+FROM station
+WHERE city LIKE 'A%'
+    OR city LIKE 'E%'
+    OR city LIKE 'I%'
+    OR city LIKE 'O%'
+    OR city LIKE 'U%';
