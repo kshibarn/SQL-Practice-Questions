61c6fb96f211b6f11736b6c64447c31a9448e26e
KSHITIJ BARNWAL <73215352+kshibarn@users.noreply.github.com>
2022-10-17 15:16:16 +0530

    Easy Leetcode Database Questions

---
+++ Leetcode/Easy/Employees_Earning_More_Than_Their_Managers.sql
@@ -0,0 +1,24 @@
+/* Table: Employee
+ 
+ +-------------+---------+
+ | Column Name | Type    |
+ +-------------+---------+
+ | id          | int     |
+ | name        | varchar |
+ | salary      | int     |
+ | managerId   | int     |
+ +-------------+---------+
+ id is the primary key column for this table.
+ Each row of this table indicates the ID of an employee, their name, salary, and the ID of their manager.
+ 
+ 
+ Write an SQL query to find the employees who earn more than their managers.
+ 
+ Return the result table in any order.
+ */
+ 
+SELECT e.name AS Employee
+FROM Employee as e
+    JOIN Employee as m
+WHERE e.managerId = m.id
+    and e.salary > m.salary;
